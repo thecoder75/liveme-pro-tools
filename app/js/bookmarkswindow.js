37f42de8d39bc19f6fb3ff1a0b6f4391e5406dc3
TheCoder75 <thecoder1975@gmail.com>
2018-02-12 21:34:59 -0500

    Added new/non-new filter button

--- app/js/bookmarkswindow.js
+++ app/js/bookmarkswindow.js
@@ -5,11 +5,22 @@ const   { ipcRenderer, remote, clipboard } = require('electron'),
         prettydate = require('pretty-date'),
         DataManager = remote.getGlobal('DataManager');
 
+var     list = [], index, max;
+
 $(function(){
     $('main').show();
+
+    list = DataManager.getAllBookmarks();
+    index = 0;
+    max = list.length;
+
+    $('#bookmark-list').html('');
+    $('footer h1').html(max + ' bookmarks listed.');
+
     setTimeout(function(){
-        redrawList();
-    }, 400);
+        drawEntry();
+    }, 5);
+
 });
 
 function closeWindow() { window.close(); }
@@ -19,88 +30,44 @@ function showFollowers(u) { ipcRenderer.send('open-followers-window', { userid: 
 function showUser(u) { ipcRenderer.send('show-user', { userid: u }); }
 
 function redrawList() {
-    var list = DataManager.getAllBookmarks();
-
-    if (list.length == 0) { 
-        return;
-    }
-
+    index = 0;
     $('#bookmark-list').html('');
-    $('footer h1').html('Reloading list...');
-
-    for (i = 0; i < list.length; i++) {
-        var d1 = prettydate.format(new Date(list[i].newest_replay * 1000));
-        var d2 = prettydate.format(new Date(list[i].last_viewed * 1000));
-        var isNew = list[i].newest_replay > list[i].last_viewed ? 'new' : '';
-
-        $('#bookmark-list').append(`
-                    <tr id="entry-${list[i].uid}" data-viewed="${list[i].last_viewed}">
-                        <td width="64">
-                            <img src="${list[i].face}" style="height: 64px; width: 64px;" onError="$(this).hide()" align="bottom">
-                        </td>
-                        <td width="90%" class="main ${isNew}">
-                            <div class="flag">NEW</div>
-                            <h1>${list[i].nickname}</h1>
-                            <h3><span>Latest Replay:</span> ${d1}</h3>
-                            <h4><span>Last Viewed:</span> ${d2}</h4>
-                            <div id="user-${list[i].uid}-buttons" class="buttons">
-                                <a class="button mini view" onClick="showUser('${list[i].uid}')">${list[i].counts.replays} replays</a>
-                                <a class="button mini fans" onClick="showFollowers('${list[i].uid}')">${list[i].counts.followers} Fans</a>
-                                <a class="button mini following" onClick="showFollowing('${list[i].uid}')">Following ${list[i].counts.followings}</a>
-                            </div>
-                        </td>
-
-                    </tr>
-        `);
-    
-        LiveMe.getUserReplays(list[i].uid, 1, 10).then(replays => {
-
-            if (replays == undefined) return;
-            if (replays.length < 1) return;
-
-            var count = 0, userid = replays[0].userid, d = $('#entry-'+userid).attr('data-viewed');
-            for (i = 0; i < replays.length; i++) {
-                if (replays[i].vtime - d > 0) count++;
-            }
-
-
-            if (count > 0) {
-                $('#entry-' + userid + ' td.main').addClass('new');
-
-                console.log(JSON.stringify(replays[0], null, 2));
-
-                var bookmark = DataManager.getSingleBookmark(userid);
-                bookmark.newest_replay = Math.floor(replays[0].vtime / 1000);
-                DataManager.updateBookmark(bookmark);
-
-            }
-
-
-        });
-
-
-    }
-    $('footer h1').html(list.length + ' bookmarks listed.');
-
-
+    drawEntry();
 }
 
-/*
+function drawEntry() {
 
-    uid: user.user_info.uid,
-    shortid: user.user_info.short_id,
-    signature: user.user_info.usign,
-    sex: sex,
-    face: user.user_info.face,
-    nickname: user.user_info.uname,
-    counts: {
-        replays: user.count_info.video_count,
-        friends: user.count_info.friends_count,
-        followers: user.count_info.follower_count,
-        followings: user.count_info.following_count,
-    },
-    last_viewed: Math.floor((new Date()).getTime() / 1000),
-    newest_replay: 0
+    if (index == max) return;
 
+    var d1 = prettydate.format(new Date(list[index].newest_replay * 1000));
+    var d2 = prettydate.format(new Date(list[index].last_viewed * 1000));
+    var isNew = list[index].newest_replay > list[index].last_viewed ? 'new' : 'not-new';
 
-*/
+    $('#bookmark-list').append(`
+                <tr id="entry-${list[index].uid}" data-viewed="${list[index].last_viewed}" class="${isNew}">
+                    <td width="64">
+                        <img src="${list[index].face}" style="height: 64px; width: 64px;" onError="$(this).hide()" align="bottom">
+                    </td>
+                    <td width="90%" class="main ${isNew}">
+                        <div class="flag">NEW</div>
+                        <h1>${list[index].nickname}</h1>
+                        <h3><span>Latest Replay:</span> ${d1}</h3>
+                        <h4><span>Last Viewed:</span> ${d2}</h4>
+                        <div id="user-${list[index].uid}-buttons" class="buttons">
+                            <a class="button mini view" onClick="showUser('${list[index].uid}')">${list[index].counts.replays} replays</a>
+                            <a class="button mini fans" onClick="showFollowers('${list[index].uid}')">${list[index].counts.followers} Fans</a>
+                            <a class="button mini following" onClick="showFollowing('${list[index].uid}')">Following ${list[index].counts.followings}</a>
+                        </div>
+                    </td>
+
+                </tr>
+    `);
+    index++;
+
+    setTimeout(function(){ drawEntry(); }, 5);
+}
+
+function hideNonRecent() {
+    $('#bookmark-list tr.not-new').toggle();
+}
+
