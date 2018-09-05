681067b58009a25bdea70e0f572167b339850d91
The Coder <30754178+thecoder75@users.noreply.github.com>
2018-09-05 13:32:01 -0400

    Fixed spelling mistake by Lewdninja

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ ipcMain.on('download-cancel', (event, arg) => {
  */
 const dlQueue = async.queue((task, done) => {
     // Set custom FFMPEG path if defined
-    if (appSettings.get('downloads.ffmepg')) ffmpeg.setFfmpegPath(appSettings.get('downloads.ffmepg'))
+    if (appSettings.get('downloads.ffmpeg')) ffmpeg.setFfmpegPath(appSettings.get('downloads.ffmpeg'))
     // Get video info
     LiveMe.getVideoInfo(task).then(video => {
         const path = appSettings.get('downloads.path')
@@ -328,134 +328,133 @@ const dlQueue = async.queue((task, done) => {
         })
 
         switch (appSettings.get('downloads.method')) {
-        case 'chunk':
-            request(video.hlsvideosource, (err, res, body) => {
-                if (err || !body) {
-                    fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify(err, null, 2))
-                    return done({ videoid: task, error: err || 'Failed to fetch m3u8 file.' })
-                }
-                // Separate ts names from m3u8
-                let concatList = ''
-                const tsList = []
-                body.split('\n').forEach(line => {
-                    if (line.indexOf('.ts') !== -1) {
-                        const tsName = line.split('?')[0]
-                        const tsPath = `${path}/lpt_temp/${video.vid}_${tsName}`
-                        // Check if TS has already been added to array
-                        if (concatList.indexOf(tsPath) === -1) {
-                            // We'll use this later to merge downloaded chunks
-                            concatList += `${tsPath}|`
-                            // Push data to list
-                            tsList.push({ name: tsName, path: tsPath })
-                        }
-                    }
-                })
-                // remove last |
-                concatList = concatList.slice(0, -1)
-                // Check if tmp dir exists
-                if (!fs.existsSync(`${path}/lpt_temp`)) {
-                    // create temporary dir for ts files
-                    fs.mkdirSync(`${path}/lpt_temp`)
-                }
-                // Download chunks
-                let downloadedChunks = 0
-                async.eachLimit(tsList, 10, (file, next) => {
-                    const stream = request(`${video.hlsvideosource.split('/').slice(0, -1).join('/')}/${file.name}`)
-                        .on('error', err => {
-                            fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify(err, null, 2))
-                            return done({ videoid: task, error: err })
-                        })
-                        .pipe(
-                            fs.createWriteStream(file.path)
-                        )
-                    // Events
-                    stream.on('finish', () => {
-                        downloadedChunks += 1
-                        mainWindow.webContents.send('download-progress', {
-                            videoid: task,
-                            state: `Downloading stream chunks.. (${downloadedChunks}/${tsList.length})`,
-                            percent: Math.round((downloadedChunks / tsList.length) * 100)
-                        })
-                        next()
-                    })
-                }, () => {
-                    // Chunks downloaded
-                    ffmpeg()
-                        .on('start', c => {
-                            console.log('started', c)
-                            mainWindow.webContents.send('download-progress', {
-                                videoid: task,
-                                state: `Converting to MP4, please wait..`,
-                                percent: 100
-                            })
-                        })
-                        .on('end', (stdout, stderr) => {
-                            if (appSettings.get('downloads.deltmp')) {
-                                tsList.forEach(file => fs.unlinkSync(file.path))
-                            }
-                            return done()
-                        })
-                        .on('error', (err, stdout, stderr) => {
-                            fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify([err, stdout, stderr], null, 2))
-                            if (appSettings.get('downloads.deltmp')) {
-                                tsList.forEach(file => fs.unlinkSync(file.path))
-                            }
-                            return done({ videoid: task, error: err })
-                        })
-                        .input(`concat:${concatList}`)
-                        .output(`${path}/${filename}`)
-                        // .outputOption('-strict -2')
-                        .outputOptions([
-                            '-c:v libx264',
-                            '-q:v 0',
-                            '-c:a copy',
-                            '-bsf:a aac_adtstoasc',
-                            '-vsync 2',
-                            '-preset superfast'
-                        ])
-                        .run()
-                })
-            })
-            break
-        case 'ffmpeg':
-            ffmpeg(video.hlsvideosource)
-                .outputOptions([
-                    '-c copy',
-                    '-bsf:a aac_adtstoasc',
-                    '-vsync 2',
-                    '-movflags faststart'
-                ])
-                .output(path + '/' + filename)
-                .on('end', function (stdout, stderr) {
-                    return done()
-                })
-                .on('progress', function (progress) {
-                    // FFMPEG doesn't always have this >.<
-                    if (!progress.percent) {
-                        progress.percent = ((progress.targetSize * 1000) / +video.videosize) * 100
-                    }
-                    mainWindow.webContents.send('download-progress', {
-                        videoid: task,
-                        state: `Downloading (${Math.round(progress.percent)}%)`,
-                        percent: progress.percent
-                    })
-                })
-                .on('start', function (c) {
-                    console.log('started', c)
-                    mainWindow.webContents.send('download-start', {
-                        videoid: task,
-                        filename: filename
-                    })
-                })
-                .on('error', function (err, stdout, stderr) {
-                    fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify([err, stdout, stderr], null, 2))
-                    return done({ videoid: task, error: err })
-                })
-                .run()
-            break
+			case 'chunk':
+				request(video.hlsvideosource, (err, res, body) => {
+					if (err || !body) {
+						fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify(err, null, 2))
+						return done({ videoid: task, error: err || 'Failed to fetch m3u8 file.' })
+					}
+					// Separate ts names from m3u8
+					let concatList = ''
+					const tsList = []
+					body.split('\n').forEach(line => {
+						if (line.indexOf('.ts') !== -1) {
+							const tsName = line.split('?')[0]
+							const tsPath = `${path}/lpt_temp/${video.vid}_${tsName}`
+							// Check if TS has already been added to array
+							if (concatList.indexOf(tsPath) === -1) {
+								// We'll use this later to merge downloaded chunks
+								concatList += `${tsPath}|`
+								// Push data to list
+								tsList.push({ name: tsName, path: tsPath })
+							}
+						}
+					})
+					// remove last |
+					concatList = concatList.slice(0, -1)
+					// Check if tmp dir exists
+					if (!fs.existsSync(`${path}/lpt_temp`)) {
+						// create temporary dir for ts files
+						fs.mkdirSync(`${path}/lpt_temp`)
+					}
+					// Download chunks
+					let downloadedChunks = 0
+					async.eachLimit(tsList, 10, (file, next) => {
+						const stream = request(`${video.hlsvideosource.split('/').slice(0, -1).join('/')}/${file.name}`)
+							.on('error', err => {
+								fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify(err, null, 2))
+								return done({ videoid: task, error: err })
+							})
+							.pipe(
+								fs.createWriteStream(file.path)
+							)
+						// Events
+						stream.on('finish', () => {
+							downloadedChunks += 1
+							mainWindow.webContents.send('download-progress', {
+								videoid: task,
+								state: `Downloading stream chunks.. (${downloadedChunks}/${tsList.length})`,
+								percent: Math.round((downloadedChunks / tsList.length) * 100)
+							})
+							next()
+						})
+					}, () => {
+						// Chunks downloaded
+						ffmpeg()
+							.on('start', c => {
+								console.log('started', c)
+								mainWindow.webContents.send('download-progress', {
+									videoid: task,
+									state: `Converting to MP4, please wait..`,
+									percent: 100
+								})
+							})
+							.on('end', (stdout, stderr) => {
+								if (appSettings.get('downloads.deltmp')) {
+									tsList.forEach(file => fs.unlinkSync(file.path))
+								}
+								return done()
+							})
+							.on('error', (err, stdout, stderr) => {
+								fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify([err, stdout, stderr], null, 2))
+								if (appSettings.get('downloads.deltmp')) {
+									tsList.forEach(file => fs.unlinkSync(file.path))
+								}
+								return done({ videoid: task, error: err })
+							})
+							.input(`concat:${concatList}`)
+							.output(`${path}/${filename}`)
+							.outputOptions([
+								'-c copy',
+								'-bsf:a aac_adtstoasc',
+								'-vsync 2',
+								'-movflags faststart'
+							])
+							.run()
+					})
+				})
+				break
+			case 'ffmpeg':
+				ffmpeg(video.hlsvideosource)
+					.outputOptions([
+						'-c copy',
+						'-bsf:a aac_adtstoasc',
+						'-vsync 2',
+						'-movflags faststart'
+					])
+					.output(path + '/' + filename)
+					.on('end', function (stdout, stderr) {
+						return done()
+					})
+					.on('progress', function (progress) {
+						// FFMPEG doesn't always have this >.<
+						if (!progress.percent) {
+							progress.percent = ((progress.targetSize * 1000) / +video.videosize) * 100
+						}
+						mainWindow.webContents.send('download-progress', {
+							videoid: task,
+							state: `Downloading (${Math.round(progress.percent)}%)`,
+							percent: progress.percent
+						})
+					})
+					.on('start', function (c) {
+						console.log('started', c)
+						mainWindow.webContents.send('download-start', {
+							videoid: task,
+							filename: filename
+						})
+					})
+					.on('error', function (err, stdout, stderr) {
+						fs.writeFileSync(`${path}/${filename}-error.log`, JSON.stringify([err, stdout, stderr], null, 2))
+						return done({ videoid: task, error: err })
+					})
+					.run()
+				break
         }
     })
 }, +appSettings.get('downloads.parallel') || 3)
+
+
 /**
  * Watch a Replay - Use either internal player or external depending on settings
  */
