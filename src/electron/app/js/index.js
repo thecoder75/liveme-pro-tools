e05669c2f0fdd9236ced904ead4ea14c4ef1a6a7
Richard <richard@optiplex7010.internal.fifelake.com>
2018-12-30 18:18:26 -0500

    Changed page size for data loading

--- src/electron/app/js/index.js
+++ src/electron/app/js/index.js
@@ -1,6 +1,5 @@
 /* global $ */
-
-const MAX_PER_PAGE = 10
+const MAX_PER_PAGE = 5
 
 const { electron, BrowserWindow, remote, ipcRenderer, shell, dialog, clipboard } = require('electron')
 const fs = require('fs')
