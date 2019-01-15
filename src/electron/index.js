thecoder75/liveme-pro-tools

commit 131e68028e87df01a85b8ef5ea8bd21e13887739

Fixed a typo found

src/electron/app/player.html
@@ -64,7 +64,7 @@
                     var hls = new Hls();
                     hls.loadSource(vid.hlsvideosource);
                     hls.attachMedia(video);
-                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                    hls.on(hls.Events.MANIFEST_PARSED, function() {
                         video.play();
                     });
                 });

commit c79ef4dbe2334d6106247d153e559e044f7c1537

Misc fixes for the video player * Fix regression introduced in 131e6802 * Use the keydown event instead of onkeypress() * Make autoplay work again by using `--autoplay-policy=no-user-gesture-required`   (https://developers.google.com/web/updates/2017/09/autoplay-policy-changes)

src/electron/app/player.html
@@ -46,10 +46,11 @@
             video.currentTime = video.currentTime - 10
         }
 
-        document.onkeypress = (e) => {
-            e = e || window.event;
-            e.code in keyMapping ? keyMapping[e.code]() : _
-        }
+        document.addEventListener('keydown', (e) => {
+            if (e.code in keyMapping) {
+                keyMapping[e.code]();
+            }
+        });
 
         $(function() {
 
@@ -66,7 +67,7 @@
                     var hls = new Hls();
                     hls.loadSource(vid.hlsvideosource);
                     hls.attachMedia(video);
-                    hls.on(hls.Events.MANIFEST_PARSED, function() {
+                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
                         video.play();
                     });
                 });

src/electron/index.js
@@ -18,6 +18,10 @@ const isDev = require('electron-is-dev')
 const ffmpeg = require('fluent-ffmpeg')
 const async = require('async')
 
+// This is required to re-enable autoplay. See:
+// https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
 let mainWindow = null
 let playerWindow = null
 let bookmarksWindow = null