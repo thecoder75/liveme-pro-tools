5e4684975b51383c73094b4f8d0b7254be28596f
TheCoder <thecoder1975@gmail.com>
2019-02-08 06:35:02 -0500

    Updated download icon, added LAMD toggle, added account lock

--- src/electron/app/js/index.js
+++ src/electron/app/js/index.js
@@ -528,6 +528,41 @@ function AddToBookmarks() {
     }
 }
 
+
+function AddToLAMD() {
+    if (DataManager.isBookmarked(currentUser) === true) {
+        if (currentUser.lamd.monitor === false) {
+            $('a.lamd').attr('title', 'Remove from LAMD monitoring').html(`<svg class="bright blue" viewBox="0 0 20 20"><path d="M10,6.978c-1.666,0-3.022,1.356-3.022,3.022S8.334,13.022,10,13.022s3.022-1.356,3.022-3.022S11.666,6.978,10,6.978M10,12.267c-1.25,0-2.267-1.017-2.267-2.267c0-1.25,1.016-2.267,2.267-2.267c1.251,0,2.267,1.016,2.267,2.267C12.267,11.25,11.251,12.267,10,12.267 M18.391,9.733l-1.624-1.639C14.966,6.279,12.563,5.278,10,5.278S5.034,6.279,3.234,8.094L1.609,9.733c-0.146,0.147-0.146,0.386,0,0.533l1.625,1.639c1.8,1.815,4.203,2.816,6.766,2.816s4.966-1.001,6.767-2.816l1.624-1.639C18.536,10.119,18.536,9.881,18.391,9.733 M16.229,11.373c-1.656,1.672-3.868,2.594-6.229,2.594s-4.573-0.922-6.23-2.594L2.41,10l1.36-1.374C5.427,6.955,7.639,6.033,10,6.033s4.573,0.922,6.229,2.593L17.59,10L16.229,11.373z"></path></svg>`)
+            currentUser.lamd.monitor = true
+            currentUser.lamd.last_checked = Math.floor((new Date()).getTime() / 1000)
+        } else {
+            $('a.lamd').attr('title', 'Monitor with LAMD').html(`<svg class="dim" viewBox="0 0 20 20"><path d="M10,6.978c-1.666,0-3.022,1.356-3.022,3.022S8.334,13.022,10,13.022s3.022-1.356,3.022-3.022S11.666,6.978,10,6.978M10,12.267c-1.25,0-2.267-1.017-2.267-2.267c0-1.25,1.016-2.267,2.267-2.267c1.251,0,2.267,1.016,2.267,2.267C12.267,11.25,11.251,12.267,10,12.267 M18.391,9.733l-1.624-1.639C14.966,6.279,12.563,5.278,10,5.278S5.034,6.279,3.234,8.094L1.609,9.733c-0.146,0.147-0.146,0.386,0,0.533l1.625,1.639c1.8,1.815,4.203,2.816,6.766,2.816s4.966-1.001,6.767-2.816l1.624-1.639C18.536,10.119,18.536,9.881,18.391,9.733 M16.229,11.373c-1.656,1.672-3.868,2.594-6.229,2.594s-4.573-0.922-6.23-2.594L2.41,10l1.36-1.374C5.427,6.955,7.639,6.033,10,6.033s4.573,0.922,6.229,2.593L17.59,10L16.229,11.373z"></path></svg>`)
+            currentUser.lamd.monitor = false
+        }
+    } else {
+        currentUser.lamd = {
+            monitor: true,
+            last_checked: Math.floor((new Date()).getTime() / 1000)
+        }
+        DataManager.addBookmark(currentUser)
+        $('a.lamd').attr('title', 'Remove from Bookmarks').html(`<svg class="bright blue" viewBox="0 0 20 20"><path d="M10,6.978c-1.666,0-3.022,1.356-3.022,3.022S8.334,13.022,10,13.022s3.022-1.356,3.022-3.022S11.666,6.978,10,6.978M10,12.267c-1.25,0-2.267-1.017-2.267-2.267c0-1.25,1.016-2.267,2.267-2.267c1.251,0,2.267,1.016,2.267,2.267C12.267,11.25,11.251,12.267,10,12.267 M18.391,9.733l-1.624-1.639C14.966,6.279,12.563,5.278,10,5.278S5.034,6.279,3.234,8.094L1.609,9.733c-0.146,0.147-0.146,0.386,0,0.533l1.625,1.639c1.8,1.815,4.203,2.816,6.766,2.816s4.966-1.001,6.767-2.816l1.624-1.639C18.536,10.119,18.536,9.881,18.391,9.733 M16.229,11.373c-1.656,1.672-3.868,2.594-6.229,2.594s-4.573-0.922-6.23-2.594L2.41,10l1.36-1.374C5.427,6.955,7.639,6.033,10,6.033s4.573,0.922,6.229,2.593L17.59,10L16.229,11.373z"></path></svg>`)
+        $('a.bookmark').attr('title', 'Remove from Bookmarks').html(`<svg class="bright yellow" viewBox="0 0 20 20"><path d="M17.684,7.925l-5.131-0.67L10.329,2.57c-0.131-0.275-0.527-0.275-0.658,0L7.447,7.255l-5.131,0.67C2.014,7.964,1.892,8.333,2.113,8.54l3.76,3.568L4.924,17.21c-0.056,0.297,0.261,0.525,0.533,0.379L10,15.109l4.543,2.479c0.273,0.153,0.587-0.089,0.533-0.379l-0.949-5.103l3.76-3.568C18.108,8.333,17.986,7.964,17.684,7.925 M13.481,11.723c-0.089,0.083-0.129,0.205-0.105,0.324l0.848,4.547l-4.047-2.208c-0.055-0.03-0.116-0.045-0.176-0.045s-0.122,0.015-0.176,0.045l-4.047,2.208l0.847-4.547c0.023-0.119-0.016-0.241-0.105-0.324L3.162,8.54L7.74,7.941c0.124-0.016,0.229-0.093,0.282-0.203L10,3.568l1.978,4.17c0.053,0.11,0.158,0.187,0.282,0.203l4.578,0.598L13.481,11.723z"></path></svg>`)
+    }
+}
+
+function toggleAccountLock() {
+    if ('locked' in currentUser) {
+        delete currentUser.locked
+        $('a.account-lock').removeClass('red')
+        $('a.account-lock svg').removeClass('bright')
+    } else {
+        currentUser.locked = true
+        $('a.account-lock').addClass('red')
+        $('a.account-lock svg').addClass('bright')
+    }
+    DataManager.updateBookmark(currentUser)
+}
+
 function backupData() {
     ipcRenderer.send('create-backup')
 
@@ -812,12 +847,14 @@ function performUserLookup(uid) {
             }
 
             allReplays = []
+            /*
             let accstatsUI = document.getElementById("variance")
             accstatsUI.style.opacity = 0.5
             accstatsUI.innerHTML = `
             <abbr title="Variance">
                 <span>Var:</span><b> - </b>
             </abbr>`
+            */
 
             getUsersReplays()
             showProgressBar()
@@ -933,12 +970,12 @@ function _addReplayEntry(replay, wasSearched) {
 
     let downloadDate = DataManager.wasDownloaded(replay.vid)
     let watchDate = DataManager.wasWatched(replay.vid)
-    let downloaded = downloadDate === false ? 'dim' : 'bright green'
+    let downloaded = downloadDate === false ? '' : 'bright green'
     let watched = watchDate === false ? 'dim' : 'bright green'
     let seen = watchDate === false ? '' : 'watched'
 
     let isLive = replay.status == 0 ? '<b style="color: limegreen;">[LIVE] </b>' : ''
-    let inQueue = $('#download-' + replay.vid).length > 0 ? 'bright' : downloaded
+    let inQueue = $('#download-' + replay.vid).length > 0 ? 'dim yellow' : downloaded
 
     const template = Handlebars.compile($('#replays-list-row').html())
 
@@ -961,6 +998,7 @@ function _addReplayEntry(replay, wasSearched) {
 
     allReplays.push(replayData)
 
+    /*
     if (allReplays.length < 2) {
         let accstatsUI = document.getElementById("variance")
         accstatsUI.style.opacity = 0.5
@@ -985,6 +1023,7 @@ function _addReplayEntry(replay, wasSearched) {
 
         }
     }
+    */
 
     const html = template(replayData)
 
