fa5a27dd5dc4020b71faa30506e645fd6986f842
TheCoder <thecoder1975@gmail.com>
2019-02-08 22:03:45 -0500

    Fixed lock issue

--- src/electron/app/js/index.js
+++ src/electron/app/js/index.js
@@ -553,16 +553,24 @@ function AddToLAMD() {
 }
 
 function toggleAccountLock() {
-    if ('locked' in currentUser) {
-        delete currentUser.locked
-        $('a.account-lock').removeClass('red')
-        $('a.account-lock svg').removeClass('bright')
-    } else {
-        currentUser.locked = true
-        $('a.account-lock').addClass('red')
-        $('a.account-lock svg').addClass('bright')
-    }
-    DataManager.updateBookmark(currentUser)
+
+    if (DataManager.isBookmarked(currentUser.uid) === true) {
+        let bm = DataManager.getSingleBookmark(currentUser.uid)
+        if (typeof bm.locked == 'undefined') {
+            bm.locked = false
+        }
+
+        if (bm.locked) {
+            $('a.account-lock').removeClass('red')
+            $('a.account-lock svg').removeClass('bright')
+            bm.locked = false
+        } else {
+            $('a.account-lock').addClass('red')
+            $('a.account-lock svg').addClass('bright')
+            bm.locked = true
+        }
+        DataManager.updateBookmark(bm)
+    }        
 }
 
 function backupData() {
@@ -812,13 +820,22 @@ function performUserLookup(uid) {
                     <path d="M17.684,7.925l-5.131-0.67L10.329,2.57c-0.131-0.275-0.527-0.275-0.658,0L7.447,7.255l-5.131,0.67C2.014,7.964,1.892,8.333,2.113,8.54l3.76,3.568L4.924,17.21c-0.056,0.297,0.261,0.525,0.533,0.379L10,15.109l4.543,2.479c0.273,0.153,0.587-0.089,0.533-0.379l-0.949-5.103l3.76-3.568C18.108,8.333,17.986,7.964,17.684,7.925 M13.481,11.723c-0.089,0.083-0.129,0.205-0.105,0.324l0.848,4.547l-4.047-2.208c-0.055-0.03-0.116-0.045-0.176-0.045s-0.122,0.015-0.176,0.045l-4.047,2.208l0.847-4.547c0.023-0.119-0.016-0.241-0.105-0.324L3.162,8.54L7.74,7.941c0.124-0.016,0.229-0.093,0.282-0.203L10,3.568l1.978,4.17c0.053,0.11,0.158,0.187,0.282,0.203l4.578,0.598L13.481,11.723z"></path>
                 </svg>
                 `)
+
+                let b = DataManager.getSingleBookmark(user.user_info.uid)
+                if (b.locked) {
+                    $('a.account-lock').addClass('red')
+                    $('a.account-lock svg').addClass('bright')
+                } else {
+                    $('a.account-lock').removeClass('red')
+                    $('a.account-lock svg').removeClass('bright')
+                }
             } else {
                 $('#user-details a.bookmark').attr('title', 'Add to Bookmarks').html(`
                 <svg class="dim" viewBox="0 0 20 20">
                     <path d="M17.684,7.925l-5.131-0.67L10.329,2.57c-0.131-0.275-0.527-0.275-0.658,0L7.447,7.255l-5.131,0.67C2.014,7.964,1.892,8.333,2.113,8.54l3.76,3.568L4.924,17.21c-0.056,0.297,0.261,0.525,0.533,0.379L10,15.109l4.543,2.479c0.273,0.153,0.587-0.089,0.533-0.379l-0.949-5.103l3.76-3.568C18.108,8.333,17.986,7.964,17.684,7.925 M13.481,11.723c-0.089,0.083-0.129,0.205-0.105,0.324l0.848,4.547l-4.047-2.208c-0.055-0.03-0.116-0.045-0.176-0.045s-0.122,0.015-0.176,0.045l-4.047,2.208l0.847-4.547c0.023-0.119-0.016-0.241-0.105-0.324L3.162,8.54L7.74,7.941c0.124-0.016,0.229-0.093,0.282-0.203L10,3.568l1.978,4.17c0.053,0.11,0.158,0.187,0.282,0.203l4.578,0.598L13.481,11.723z"></path>
                 </svg>
                 `)
-            }
+            }        
 
             $('#user-details div.info a.following').html(`Following ${user.count_info.following_count}`)
             $('#user-details div.info a.followers').html(`${user.count_info.follower_count} Fans`)
