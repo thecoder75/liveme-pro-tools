c7a680924ad7ac01bf3ab1dab4ffcd2eb359d8d1
The Coder <thecoder1975@gmail.com>
2021-12-23 06:18:56 -0500

    Switched from original planned W2UI to Metro UI

--- src/electron/app/js/start.js
+++ src/electron/app/js/start.js
@@ -4,3 +4,40 @@ Filename: start.js
 Description: Handle startup of the app and the first window code to allow selection of desired action(s)
 
 */
+
+
+
+
+function showDownloads() { ipcRenderer.send('show-download-window') }
+function showOptions() { ipcRenderer.send('show-options-window') }
+
+function setupIPCListeners() {
+    ipcRenderer.on('show-user', (event, arg) => {
+    })
+    ipcRenderer.on('show-user', (event, arg) => {
+    })
+
+}
+
+function executeSearch() {
+    $('#stype').attr('disabled','disabled');
+    $('#sdata').attr('disabled','disabled');
+    $('#search-query').val(arg.userid)
+    $('#search-type').val('user-id')
+
+
+
+
+}
+
+function renderResults() {
+
+
+
+}
+
+
+$(function() {
+    setupIPCListeners();
+    $('#overlay').hide();
+})
