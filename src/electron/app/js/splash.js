815e813458aa835b2b56aebe5c2971413e298da5
The Coder <thecoder1975@gmail.com>
2021-12-28 12:20:05 -0500

    Added comment

--- src/electron/app/js/splash.js
+++ src/electron/app/js/splash.js
@@ -1,3 +1,10 @@
+/*
+
+Filename: splash.js
+Description: Handle the splash dialog on startup
+
+*/
+
 $(function() {
     const u1=window.location.href.split('?')[1]
     $('h6').html('v' + u1.split('=')[1])
